d5b0edd584cd5b7e07c5b9e7df68e99472b31caf
Rs Sabbir Ahmed <rssabbirahmed1996@gmail.com>
2019-01-19 23:18:30 +0600

    fun 2

---
+++ function.js
@@ -0,0 +1,177 @@
+// let a = 5;
+// let b = 3;
+// function sum(a,b) {
+//   let c =a + b;
+//   let d = a-b;
+//   console.log(c);
+//   console.log(d);
+  
+  
+// }
+// sum(a,b);
+// sum(13,34);
+
+// example 2
+// function myFn(a , b) {
+//   let c;
+//   a = a+1;
+//   c = a + b;
+//   return c;
+// }
+
+// console.log(myFn(41, 12));
+
+
+/**
+ * EXAMPLE 2
+ */
+
+// function myfunction(a,b) {
+//   console.log(a);
+//   console.log(b); 
+//   return a; 
+
+// }
+// myfunction("j ",'h ');
+// console.log(myfunction("6", '9 '));
+
+
+/**
+ * EXAMPLE 3
+ * 
+ * Function scope
+ */
+// function myFunction(a,b) {
+//    console.log(a,b);
+// }
+// console.log(myFunction(3,2));
+
+// // console.log(a);
+// // console.log(b);
+
+
+/**
+ * EXAMPLE 4
+ * 
+ * Reuse parameter names
+ */
+// function myFunction1(a,b) {
+//   console.log(a,b);
+  
+// }
+// function myFunction2(a,b) {
+//   console.log(a,b);
+  
+// }
+
+// const a = true;
+// const b = null;
+
+
+// myFunction1(2,3);
+// myFunction2(5,7);
+
+// console.log(a);
+// console.log(b);
+// console.log(a,b);
+
+
+/**
+ * EXAMPLE 5
+ * 
+ * Add return
+ */
+// function sum(a,b) {
+//   return a + b;
+// }
+// sum(10,5);
+// console.log(sum(2,5));
+// console.log(sum(10,3));
+// console.log(sum("abe",2));
+// console.log(sum());
+
+
+
+/**
+ * EXAMPLE 6
+ * 
+ * What happens after "return"?
+ */
+// function myFunction(a) {
+//   console.log(a);
+//    return a; // function stops after return statement
+//   console.log(a);
+//   const c = 20;
+//   console.log(c);
+  
+// }
+// myFunction(10);
+
+
+/**
+ * CHALLENGE 1
+ * 
+ * Create function called "mult" and it will have 3 parameters.
+ * Create new variable and assign to it result of multiplication af al 3 parameters.
+ * Print to the console result.
+ * Dont't use "return".
+ */
+
+//  function mult(a,b,c) {
+//   let d = a*b*c;
+//    console.log(d);
+//   //  return d;
+//  }
+//  mult();
+// mult(2, 3, 5);
+// mult(2, 3, "abc");
+// // console.log(mult(2,3,4));
+
+
+/**
+ * CHALLENGE 2
+ * 
+ * Crate a function "concatenateSrtings" and it will have 2 parameters.
+ * And this function will return concatenated string.
+ */
+
+// function concatenateSrtings(a,b) {
+//    return a+b;
+   
+//  }
+// console.log(concatenateSrtings("Hello ", "World "));
+
+
+/**
+ * CHALLENGE 3
+ * 
+ * Create function "outerFunction" with 2 parameters.
+ * Create function inside of the "outerFunction" and name it "innerFunction" with one parameter.
+ * This "innerFunction" will return square of parameter(paremeter * parameter).
+ * 
+ * In thi "outerFunction" sum both parameters.
+ * Call "innerFunction" with argument that is epual to sum of both parameters of the "outerFunction".
+ * Log to the console result of the "innerFunction" call.
+ */
+// solve 1===================
+//  function outerFunction(a,b) {
+//    const sum = a+b;
+//    function innerFunction(squar) {
+//      return squar * squar;
+//     }
+//     const result = innerFunction(sum);
+//    console.log(result);
+//  }
+//    outerFunction(2,8);
+
+
+// Solve Simplified ========================
+
+
+function outerFunction(a, b) {
+  function innerFunction(squar) {
+    return squar * squar;
+  }
+  console.log(innerFunction(a + b));
+}
+outerFunction(2, 8);
