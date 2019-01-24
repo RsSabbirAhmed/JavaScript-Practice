0ae8bf17048b6593d271f7f7d9b372f1f9c2fde2
Rs Sabbir Ahmed <rssabbirahmed1996@gmail.com>
2019-01-24 19:29:48 +0600

    expression and statement

---
+++ expressions.js
@@ -0,0 +1,41 @@
+// /**
+//  * EXPRESSIONS
+//  */
+// 10;
+// "abc"
+// 5 + 3;
+
+// (function (a) {
+//   console.log(a);
+  
+// });
+
+// // Immediately Invoked Function Expression
+//   (function () {
+//   console.log("Hello from IIFE");
+  
+// })();
+
+
+
+/**
+ * STATEMENTS
+ */
+
+ let a; //Variable declaration is statement
+
+ a = 3
+
+ function myFunction(a) {
+   return a;
+ }
+ myFunction(a);
+
+
+
+/**
+ *JS-BASICS/CORE/CHALLENGES/03-EXPRESSIONS-V-S-STATEMENT
+ * 
+ * 
+ */
+
