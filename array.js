39f3296167fa090b3639139f11deaa5439a40034
Rs Sabbir Ahmed <rssabbirahmed1996@gmail.com>
2019-01-27 21:07:26 +0600

    v.3

---
+++ array.js
@@ -0,0 +1,168 @@
+// const arr = [1, 2, 3];
+// console.log(arr);
+
+
+
+
+
+// const myArray = [1, 2, 3, 4];
+// console.log(myArray);
+// console.log(myArray.length);
+
+// myArray[2] = "abc";
+
+// console.log(myArray);
+// console.log(myArray[2]);
+
+// myArray[4] = true;
+
+// console.log(myArray);
+// console.log(myArray.length);
+
+
+
+// Array methods ========================
+/**
+ * "PUSH"  add new element end of the array
+ * "POP" remove last element of the arr 
+ * "SHIFT" remove first element of the array
+ *"UNSHIFT"  add new element at the beginning of the array
+ */
+
+
+// // PUSH METHOD
+
+// const myArray = [1, 2, 3];
+// console.log(myArray);
+
+// myArray.push(4);
+
+// console.log(myArray);
+
+// myArray.push(true);
+
+// console.log(myArray);
+
+
+
+
+
+// // POP METHOD
+
+// const myArray2 = [1, 2, 3];
+// console.log(myArray2);
+
+// myArray2.pop();
+// console.log(myArray2);
+
+
+// const removedElement = myArray2.pop();
+
+// console.log(myArray2);
+// console.log(removedElement);
+
+
+
+
+
+// // SHIFT METHOD
+
+// const myArray3 = [1, 2, 3];
+// console.log(myArray3);
+
+// myArray3.shift();
+// console.log(myArray3);
+
+// const removedElement2 = myArray3.shift();
+
+// console.log(myArray3);
+// console.log(removedElement2);
+
+
+
+
+
+// // UNSHIFT METHOD
+
+// const myArray4 = [1, 2, 3];
+// console.log(myArray4);
+
+// myArray4.unshift(0);
+
+// console.log(myArray4);
+
+// myArray4.unshift("abc");
+// console.log(myArray4);
+
+
+
+
+
+
+// /**
+//  * EXAMPLE 1
+//  * 
+//  * Empty Array
+//  */
+
+//  const myArray = [];
+//  console.log(myArray);
+ 
+
+
+
+
+// /**
+//  * EXAMPLE 2
+//  * 
+//  * Add elements to the Array
+//  */
+
+//  const myArray = [];
+//  myArray[0] ="First Element";
+//  console.log(myArray);
+ 
+
+//  myArray[1] = "Second Element";
+//  console.log(myArray);
+ 
+//  myArray.push('Third Element'); // Preferable methods for add a new element
+//  console.log(myArray);
+ 
+
+
+
+// /**
+//  * EXAMPLE 3
+//  * 
+//  * Modify Elements
+//  * 
+//  */
+
+// const myArray = [3, true, "abc", {}];
+
+// console.log(myArray);
+// myArray[1] = "New value for second element";
+
+// console.log(myArray);
+
+// myArray[0] = null;
+// console.log(myArray);
+
+
+
+
+// myArray[3].newProp = 10;
+// console.log(myArray);
+// console.log(myArray[3].newProp);
+// console.log(myArray[3]["newProp"]);
+
+
+
+
+
+/**
+ * EXAMPLE 4
+ * 
+ * Square bracket notation
+ */
