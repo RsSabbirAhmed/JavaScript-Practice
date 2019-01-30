12e776593ffb3b063ca2e25be5ae52a0ec0fef1c
Rs Sabbir Ahmed <rssabbirahmed1996@gmail.com>
2019-01-30 07:28:26 +0600

    v-22

---
+++ loops.js
@@ -0,0 +1,102 @@
+// let i =0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+
+
+
+
+
+// // For loop =====
+//  for(let i = 0; i <5; i++){
+//    console.log(i);
+   
+//  }
+
+
+
+
+
+
+
+// // While loop==========
+// while (condition) {
+//   // Code that is executed on each iteration
+// }
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+
+
+
+
+
+
+// // Do while loop ==============
+
+// // do{
+// //   // code that is executed on each iteration
+// // } while (condition);
+
+// let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+  
+// } while (i < 5);
+
+
+
+
+
+
+// // "For in" loop =========================
+
+
+// // for object=====
+
+
+// // const myObject = {
+// //   x: 10,
+// //   y: true,
+// //   z: "abc"
+// // }
+// // for (const sabbir in myObject) {
+// //   console.log(sabbir,myObject[sabbir]);
+  
+// // }
+
+
+// // for array====
+// const myArray = [true, 10, "abc", null];
+
+// for (const key in myArray) {
+//   console.log(key,myArray[key]);
+  
+// }
+
+
+
+
+// // "for of loop" ========================
+
+// // for (Element of Iterable) {
+//   // Actions with specific element
+// // }
+
+
+// const myArray = [true, 10, "abc", null];
+
+// for (const element of myArray) {
+//   console.log(element);
+  
+// }// this loop can't be used with objects,this is only for Arrays
+
+// // For of loop is not able to access with "objects" because object are not iterable
