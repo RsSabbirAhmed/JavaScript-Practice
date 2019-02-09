fd136b3ebb6a6ba644929d73597223879831acbb
Rs Sabbir Ahmed <rssabbirahmed1996@gmail.com>
2019-02-10 01:37:18 +0600

    es-6

--- ES-6/Arrow fn/thiskeyword.js
+++ ES-6/Arrow fn/thiskeyword.js
@@ -13,11 +13,22 @@
 
 // Arrow function doesn't have it's own "this", and that's why when arrow function use "this" keyword is indicates outer scope. And outer scope is "window" object.
 
+// const num = {
+//   value: 100,
+//   info:() => {
+//     console.log(this); // window
+//     return this.value;
+//   }
+// };
+// console.log(num.info());
+
+// EXAMPLE 1
 const num = {
   value: 100,
-  info:() => {
-    console.log(this); // window
+  info: function info() {
     return this.value;
+    
   }
-};
+}
 console.log(num.info());
+
